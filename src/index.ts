cafbe1a81db67e493e8822672340228bff13d200
shreekrishnalamichhane <uniksk123@gmail.com>
2022-12-17 18:37:20 +0545

    V 1.4.0 : One browser, multipage mode

--- src/index.ts
+++ src/index.ts
@@ -8,6 +8,7 @@ dotenv.config()
 import Log from "./helpers/log";
 import PingPongController from "./controllers/PingPongController";
 import JobController from "./controllers/JobController";
+import puppeteer, { Browser } from "puppeteer";
 /* Custom Libraries End */
 
 /* Custom Configurations Start */
@@ -16,17 +17,31 @@ const socket = ioClient(MASTER_URL, {
     path: '/'
 });
 
-/* Custom Configurations End */
+(async () => {
+    const browser: Browser = await puppeteer.launch({
+        headless: true,
+        args: ["--no-sandbox", "--disabled-setupid-sandbox"],
+        ignoreDefaultArgs: ['--disable-extensions'],
+        // executablePath: "/usr/bin/chromium-browser"
+    });
+    /* Custom Configurations End */
 
-socket.on('connect', () => {
-    Log.log('Connect', `Connected to Master with id :${socket.id}`);
+    socket.on('connect', () => {
+        Log.log('Connect', `Connected to Master with id :${socket.id}`);
 
-    PingPongController.pingMaster(socket);
-});
+        PingPongController.pingMaster(socket);
+    });
 
-socket.on('ping', () => PingPongController.pongMaster(socket));
-socket.on('pong', () => JobController.requestJob(socket));
-socket.on('dispatch_ready', () => JobController.availableJob(socket))
-socket.on('dispatch', (job) => JobController.gotJob(socket, job))
-socket.on('processed', () => JobController.requestJob(socket))
-socket.on('disconnect', () => PingPongController.whenDisconnected(socket));
+    socket.on('ping', () => PingPongController.pongMaster(socket));
+    socket.on('pong', () => JobController.requestJob(socket));
+    socket.on('dispatch_ready', () => JobController.availableJob(socket))
+    socket.on('dispatch', (job) => JobController.gotJob(socket, browser, job))
+    socket.on('processed', () => JobController.requestJob(socket))
+    socket.on('disconnect', () => PingPongController.whenDisconnected(socket));
+
+    process.on("exit", () => {
+        console.log("Closing browser")
+        browser.close()
+    })
+})()
+
