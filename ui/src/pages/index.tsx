d037ac2c79a67e323b5675446d3f923a075f7f9d
suparthghimire <suparthnarayanghimire2014@gmail.com>
2023-02-01 20:50:16 +0545

    Change Root of Project and Add UI template

---
+++ ui/src/pages/index.tsx
@@ -0,0 +1,14 @@
+import Head from "next/head";
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Search</title>
+      </Head>
+      <main>
+        <button>Hello world!</button>
+      </main>
+    </>
+  );
+}
