a0ba9ead7684cdab7e0a969b985fc4975b565b68
suparthghimire <suparthnarayanghimire2014@gmail.com>
2023-03-03 00:40:04 +0545

    Move Everything inside Server Folder

--- scraper-master/src/@types/types.ts
+++
@@ -1,5 +0,0 @@
-export type TypeJob = {
-    url: String,
-    status: "free" | "reserved" | "done" | "failed",
-    contentHashId: String
-}
--- scraper-master/src/helpers/runtime.ts
+++
@@ -1,7 +0,0 @@
-import dotenv from "dotenv"
-dotenv.config()
-
-const RuntimeHelper = {
-    env: process.env.NODE_ENV || "production"
-}
-export default RuntimeHelper;
