37d40b994dd3431d97f8df4bebdf1eebef2339f8
suparthghimire <suparthnarayanghimire2014@gmail.com>
2023-03-03 00:40:04 +0545

    Move Everything inside Server Folder

--- scraper-master/prisma/migrations/20221213081702_add_metrics_to_jobs_table/migration.sql
+++
@@ -1,5 +0,0 @@
--- AlterTable
-ALTER TABLE `Job` ADD COLUMN `clientEnd` DATETIME(3) NULL,
-    ADD COLUMN `clientStart` DATETIME(3) NULL,
-    ADD COLUMN `serverEnd` DATETIME(3) NULL,
-    ADD COLUMN `serverStart` DATETIME(3) NULL;
